fea00c8b35727fcc337e8b35ff6c3291b279a73d
TangMartin <62964506+TangMartin@users.noreply.github.com>
2021-01-02 18:57:28 -0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,57 +1,66 @@
-#SERVOS
-#Port 1 (Rotation) - Right Drive
-#Port 2 (Position) - Lift (Mouth)
-#Port 3 - N/A
-#Port 4 - (Rotation) - Left Drive
-
-#SENSORS
-#Port 1 (Ultrasonic Sensor) - HC-SR04
-
-#TRI-COLOURS LEDS
-#Port 1 - Right Eye
-#Port 2 - lEft Eye
-
-def scan(velL, velR, cmdistance):
+def movetoobject(velL: number, velR: number, time: number):
     hummingbird.start_hummingbird()
-    hummingbird.set_rotation_servo(FourPort.ONE, velR)
-    hummingbird.set_rotation_servo(FourPort.FOUR, velL)
-    while hummingbird.get_sensor(SensorType.DISTANCE, ThreePort.ONE) > cmdistance:
-        basic.pause(0.1)
+    hummingbird.set_rotation_servo(FourPort.ONE, velL)
+    hummingbird.set_rotation_servo(FourPort.FOUR, velR)
+    basic.pause(time)
     hummingbird.set_rotation_servo(FourPort.ONE, 0)
     hummingbird.set_rotation_servo(FourPort.FOUR, 0)
-    
-def movetoobject(velL, velR, cmdistance):
+# SERVOS
+# Port 1 (Rotation) - Right Drive
+# Port 2 (Position) - Lift Right
+# Port 3 - (Position) - Lift Left
+# Port 4 - (Rotation) - Left Drive
+# SENSORS
+# Port 1 (Ultrasonic Sensor) - HC-SR04
+# TRI-COLOURS LEDS
+# Port 1 - Right Eye
+# Port 2 - Left Eye
+def scan(velL: number, velR: number, time: number):
     hummingbird.start_hummingbird()
-    hummingbird.set_rotation_servo(FourPort.ONE, velR)
-    hummingbird.set_rotation_servo(FourPort.FOUR, velL)
-    while hummingbird.get_sensor(SensorType.DISTANCE, ThreePort.ONE) > cmdistance:
-        basic.pause(0.1)
+    hummingbird.set_rotation_servo(FourPort.ONE, velL)
+    hummingbird.set_rotation_servo(FourPort.FOUR, velR)
+    basic.pause(time)
     hummingbird.set_rotation_servo(FourPort.ONE, 0)
     hummingbird.set_rotation_servo(FourPort.FOUR, 0)
-
-def consume(velL, velR, liftRest, liftUp, cmdistance, R, G, B):
+def consume(velL: number, velR: number, liftL: number, liftR: number, time: number, R: number, G: number, B: number):
     hummingbird.start_hummingbird()
-    Red = (R / 255) * 100
-    Green = (G / 250) * 100
-    Blue = (B / 250) * 100
-    hummingbird.set_position_servo(FourPort.TWO, liftUp)
-    hummingbird.set_tri_led(TwoPort.ONE, Red, Green, Blue)
-    hummingbird.set_tri_led(TwoPort.TWO, Red, Green, Blue)
-    hummingbird.set_rotation_servo(FourPort.ONE, velR)
-    hummingbird.set_rotation_servo(FourPort.FOUR, velL)
-    while hummingbird.get_sensor(SensorType.DISTANCE, ThreePort.ONE) > cmdistance:
-        basic.pause(0.1)
-    hummingbird.set_rotation_servo(FourPort.ONE, 0)
-    hummingbird.set_rotation_servo(FourPort.FOUR, 0)
-    hummingbird.set_position_servo(FourPort.TWO, liftRest)
-    hummingbird.set_tri_led(TwoPort.ONE, 100, 100, 100)
-    hummingbird.set_tri_led(TwoPort.TWO, 100, 100, 100)
-
-def object():
+    hummingbird.set_tri_led(TwoPort.ONE, R, G, B)
+    hummingbird.set_tri_led(TwoPort.TWO, R, G, B)
     hummingbird.set_position_servo(FourPort.TWO, 0)
+    hummingbird.set_position_servo(FourPort.THREE, 0)
+    basic.pause(time)
     hummingbird.set_tri_led(TwoPort.ONE, 100, 100, 100)
     hummingbird.set_tri_led(TwoPort.TWO, 100, 100, 100)
-    scan(20, 20, 4)
-    consume(5, 5, 0, 30, 0, 120, 120, 120)
+    hummingbird.set_position_servo(FourPort.TWO, 180)
+    hummingbird.set_position_servo(FourPort.THREE, -180)
+    hummingbird.set_rotation_servo(FourPort.ONE, velL)
+    hummingbird.set_rotation_servo(FourPort.FOUR, velR)
+    basic.pause(2000)
+    hummingbird.set_rotation_servo(FourPort.ONE, 0)
+    hummingbird.set_rotation_servo(FourPort.FOUR, 0)
+hummingbird.start_hummingbird()
+basic.show_leds("""
+    # # # # #
+    # . . . .
+    # . # # #
+    # . . . #
+    # # # # #
+    """)
+basic.pause(5000)
+hummingbird.set_tri_led(TwoPort.ONE, 100, 100, 100)
+hummingbird.set_tri_led(TwoPort.TWO, 100, 100, 100)
+scan(50, 50, 1600)
+hummingbird.set_tri_led(TwoPort.ONE, 100, 100, 100)
+hummingbird.set_tri_led(TwoPort.TWO, 100, 100, 100)
+movetoobject(102, -100, 3000)
+hummingbird.set_tri_led(TwoPort.ONE, 100, 100, 100)
+hummingbird.set_tri_led(TwoPort.TWO, 100, 100, 100)
+consume(-60,40,150,30,5000,100,0,0)
+basic.show_leds("""
+    # # # . .
+    # . . # .
+    # . . . #
+    # . . # .
+    # # # . .
+    """)
 
-object()
--- main.ts
+++ main.ts
@@ -1,61 +1,71 @@
-// SERVOS
-// Port 1 (Rotation) - Right Drive
-// Port 2 (Position) - Lift (Mouth)
-// Port 3 - N/A
-// Port 4 - (Rotation) - Left Drive
-// SENSORS
-// Port 1 (Ultrasonic Sensor) - HC-SR04
-// TRI-COLOURS LEDS
-// Port 1 - Right Eye
-// Port 2 - lEft Eye
-function scan(velL: number, velR: number, cmdistance: number) {
+function movetoobject(velL: number, velR: number, time: number) {
     hummingbird.startHummingbird()
-    hummingbird.setRotationServo(FourPort.One, velR)
-    hummingbird.setRotationServo(FourPort.Four, velL)
-    while (hummingbird.getSensor(SensorType.Distance, ThreePort.One) > cmdistance) {
-        basic.pause(0.1)
-    }
+    hummingbird.setRotationServo(FourPort.One, velL)
+    hummingbird.setRotationServo(FourPort.Four, velR)
+    basic.pause(time)
     hummingbird.setRotationServo(FourPort.One, 0)
     hummingbird.setRotationServo(FourPort.Four, 0)
 }
 
-function movetoobject(velL: number, velR: number, cmdistance: any) {
+//  SERVOS
+//  Port 1 (Rotation) - Right Drive
+//  Port 2 (Position) - Lift Right
+//  Port 3 - (Position) - Lift Left
+//  Port 4 - (Rotation) - Left Drive
+//  SENSORS
+//  Port 1 (Ultrasonic Sensor) - HC-SR04
+//  TRI-COLOURS LEDS
+//  Port 1 - Right Eye
+//  Port 2 - Left Eye
+function scan(velL: number, velR: number, time: number) {
     hummingbird.startHummingbird()
-    hummingbird.setRotationServo(FourPort.One, velR)
-    hummingbird.setRotationServo(FourPort.Four, velL)
-    while (hummingbird.getSensor(SensorType.Distance, ThreePort.One) > cmdistance) {
-        basic.pause(0.1)
-    }
+    hummingbird.setRotationServo(FourPort.One, velL)
+    hummingbird.setRotationServo(FourPort.Four, velR)
+    basic.pause(time)
     hummingbird.setRotationServo(FourPort.One, 0)
     hummingbird.setRotationServo(FourPort.Four, 0)
 }
 
-function consume(velL: number, velR: number, liftRest: number, liftUp: number, cmdistance: number, R: number, G: number, B: number) {
+function consume(velL: number, velR: number, liftL: number, liftR: number, time: number, R: number, G: number, B: number) {
     hummingbird.startHummingbird()
-    let Red = R / 255 * 100
-    let Green = G / 250 * 100
-    let Blue = B / 250 * 100
-    hummingbird.setPositionServo(FourPort.Two, liftUp)
-    hummingbird.setTriLED(TwoPort.One, Red, Green, Blue)
-    hummingbird.setTriLED(TwoPort.Two, Red, Green, Blue)
-    hummingbird.setRotationServo(FourPort.One, velR)
-    hummingbird.setRotationServo(FourPort.Four, velL)
-    while (hummingbird.getSensor(SensorType.Distance, ThreePort.One) > cmdistance) {
-        basic.pause(0.1)
-    }
-    hummingbird.setRotationServo(FourPort.One, 0)
-    hummingbird.setRotationServo(FourPort.Four, 0)
-    hummingbird.setPositionServo(FourPort.Two, liftRest)
-    hummingbird.setTriLED(TwoPort.One, 100, 100, 100)
-    hummingbird.setTriLED(TwoPort.Two, 100, 100, 100)
-}
-
-function object() {
+    hummingbird.setTriLED(TwoPort.One, R, G, B)
+    hummingbird.setTriLED(TwoPort.Two, R, G, B)
     hummingbird.setPositionServo(FourPort.Two, 0)
+    hummingbird.setPositionServo(FourPort.Three, 0)
+    basic.pause(time)
     hummingbird.setTriLED(TwoPort.One, 100, 100, 100)
     hummingbird.setTriLED(TwoPort.Two, 100, 100, 100)
-    scan(20, 20, 4)
-    consume(5, 5, 0, 30, 0, 120, 120, 120)
+    hummingbird.setPositionServo(FourPort.Two, 180)
+    hummingbird.setPositionServo(FourPort.Three, -180)
+    hummingbird.setRotationServo(FourPort.One, velL)
+    hummingbird.setRotationServo(FourPort.Four, velR)
+    basic.pause(2000)
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Four, 0)
 }
 
-object()
+hummingbird.startHummingbird()
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    # . # # #
+    # . . . #
+    # # # # #
+    `)
+basic.pause(5000)
+hummingbird.setTriLED(TwoPort.One, 100, 100, 100)
+hummingbird.setTriLED(TwoPort.Two, 100, 100, 100)
+scan(50, 50, 1600)
+hummingbird.setTriLED(TwoPort.One, 100, 100, 100)
+hummingbird.setTriLED(TwoPort.Two, 100, 100, 100)
+movetoobject(102, -100, 3000)
+hummingbird.setTriLED(TwoPort.One, 100, 100, 100)
+hummingbird.setTriLED(TwoPort.Two, 100, 100, 100)
+consume(-60, 40, 150, 30, 5000, 100, 0, 0)
+basic.showLeds(`
+    # # # . .
+    # . . # .
+    # . . . #
+    # . . # .
+    # # # . .
+    `)
